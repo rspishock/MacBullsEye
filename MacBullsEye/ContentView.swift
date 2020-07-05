c42f4fbd20503e1c8e1e8a638d331d2f7bf8480a
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-07-05 15:17:55 -0400

    Initial Commit

---
+++ MacBullsEye/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacBullsEye
+//
+//  Created by Ryan Spishock on 7/5/20.
+//  Copyright © 2020 Ryan Spishock. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
